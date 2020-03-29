eb528a17757fa95d5c00fa88fcd7fdda7f49f94e
Daniel Shu <danielshu98@gmail.com>
2020-03-28 21:14:42 -0700

    login wip

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,30 +1,106 @@
-import React from 'react';
-import { Component } from 'react';
-import { Button, FormGroup, Input, InputLabel } from "@material-ui/core";
-import logo from '../logo.svg';
-import './Login.css';
+import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 class Login extends Component {
     constructor(props) {
-        super(props);
+        super(props)
 
-        this.state = { 
-        };
+        this.state = {};
     }
 
     render() {
         return (
-            <div className="Login">
-                <FormGroup controlId="username" bsSize="large">
-                    <InputLabel htmlFor="username-input">Username</InputLabel>
-                    <Input id="username-input" aria-describedby="my-username-input" />
-                </FormGroup>
-                <FormGroup controlId="password" bsSize="large">
-                    <InputLabel htmlFor="password-input">Password</InputLabel>
-                    <Input id="password-input" aria-describedby="my-password-input" />
-                </FormGroup>
-                <Button className="Login-btn" variant="contained">Submit</Button>
+        <Grid container component="main" className="login-root">
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className="login-image" />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <div className="login-paper">
+                <Avatar className="login-avatar">
+                <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                Sign in
+                </Typography>
+                <form className="login-form" noValidate>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                />
+                <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className="login-submit"
+                >
+                    Sign In
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                    <Link href="#" variant="body2">
+                        Forgot password?
+                    </Link>
+                    </Grid>
+                    <Grid item>
+                    <Link href="#" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
+                    </Grid>
+                </Grid>
+                <Box mt={5}>
+                    <Copyright />
+                </Box>
+                </form>
             </div>
+            </Grid>
+        </Grid>
         );
     }
 }
