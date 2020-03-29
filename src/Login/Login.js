a2fbae5589ec53f9e4b68efe4697889234a40d3b
Daniel Shu <danielshu98@gmail.com>
2020-03-28 20:31:11 -0700

    created login form

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Component } from 'react';
+import { Button, FormGroup, Input, InputLabel } from "@material-ui/core";
 import logo from '../logo.svg';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+        };
+    }
 
     render() {
         return (
-             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    Learn React
-                    </a>
-                </header>
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="username" bsSize="large">
+                        <InputLabel htmlFor="username-input">Username</InputLabel>
+                        <Input id="username-input" aria-describedby="my-username-input" />
+
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                        <InputLabel htmlFor="password-input">Password</InputLabel>
+                        <Input id="password-input" aria-describedby="my-password-input" />
+                    </FormGroup>
+                    <Button variant="contained">Submit</Button>
+                </form>
             </div>
         );
     }
