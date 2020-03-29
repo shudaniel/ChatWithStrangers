aa65c58ef982ed961d8c649442eaf4863137cc46
Daniel Shu <danielshu98@gmail.com>
2020-03-28 20:46:08 -0700

    Added css

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import { Button, FormGroup, Input, InputLabel } from "@material-ui/core";
 import logo from '../logo.svg';
+import './Login.css';
 
 class Login extends Component {
     constructor(props) {
@@ -14,18 +15,15 @@ class Login extends Component {
     render() {
         return (
             <div className="Login">
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="username" bsSize="large">
-                        <InputLabel htmlFor="username-input">Username</InputLabel>
-                        <Input id="username-input" aria-describedby="my-username-input" />
-
-                    </FormGroup>
-                    <FormGroup controlId="password" bsSize="large">
-                        <InputLabel htmlFor="password-input">Password</InputLabel>
-                        <Input id="password-input" aria-describedby="my-password-input" />
-                    </FormGroup>
-                    <Button variant="contained">Submit</Button>
-                </form>
+                <FormGroup controlId="username" bsSize="large">
+                    <InputLabel htmlFor="username-input">Username</InputLabel>
+                    <Input id="username-input" aria-describedby="my-username-input" />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <InputLabel htmlFor="password-input">Password</InputLabel>
+                    <Input id="password-input" aria-describedby="my-password-input" />
+                </FormGroup>
+                <Button className="Login-btn" variant="contained">Submit</Button>
             </div>
         );
     }
