d15ba039a860bf3199b98218024570b0ffcf83d8
meredithxu <yiyangxu@umail.ucsb.edu>
2020-03-28 20:06:38 -0700

    WIP sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+
+    render() {
+        return (
+            <div className="sidebar-con">
+                <div className="profile">
+                  profile
+                </div>          
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
