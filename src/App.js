320f0285c1fbbd6ebf61142d9da2c35d3eff18be
Daniel Shu <danielshu98@gmail.com>
2020-03-31 00:21:17 -0700

    Created basic login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Component } from 'react';
 import './App.css';
-import StubHome from './StubHome'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import StubHome from './StubHome';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withFirebase } from './Firebase';
+import LoginPage from './Login/LoginPage';
 
 
 import * as ROUTES from './constants/Routes';
@@ -11,17 +13,33 @@ class App extends Component {
   
   constructor() {
     super();
+
+    this.state = {
+      authStatusReported: false,
+      isUserSignedIn: false,
+    }
   } 
 
+  componentDidMount() {
+    this.props.firebase.auth.onAuthStateChanged(user => this.setState({
+      authStatusReported: true,
+      isUserSignedIn: !!user
+    }));
+  }
+
   render() {
-    return (
+    if (this.state.isUserSignedIn) {
+        return (
           <Router>
             <div>
               <Route exact path={ROUTES.HOME} component={StubHome} />
             </div>
           </Router>
-      );
+        );
+    }
+    return <LoginPage />;
+
   }
 }
 
-export default App;
+export default withFirebase(App);
